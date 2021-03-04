2a39ee2bc137e77959de5957f7bc5f2ac41deffe
LinXiaoYi <874183200@qq.com>
2021-03-04 23:21:19 +0800

    [ADD]初始提交

---
+++ src/config/apiUtils.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'http://localhost:7000/'
+
+export const register = baseUrl + "/identity/register";
+export const login = baseUrl + "/identity/login";
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router).use(store)
+app.use(ElementPlus)
+app.mount('#app')
