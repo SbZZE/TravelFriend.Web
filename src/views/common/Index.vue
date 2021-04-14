86a7daf086e34b1070389cfd00d31717e17f952e
Bodyfunk <616982274@qq.com>
2021-04-15 00:02:01 +0800

    [ADD]首页导航栏和侧边栏的样式雏形

--- src/views/common/Index.vue
+++ src/views/common/Index.vue
@@ -1,5 +1,29 @@
 <template>
-    <div class="index">首页</div>
+<head>
+    <title>首页</title>
+</head>
+
+<body>
+    <!-- 导航栏 -->
+    <header>
+        <div class="logo">TravelFriend</div>
+        <nav>
+            <a href="#">首页</a>
+            <a href="#">个人中心</a>
+            <a href="#">相册</a>
+            <a href="#">队伍</a>
+        </nav>
+    </header>
+
+    <!-- 侧边栏容器 -->
+    <div class="sidebar">
+        <a href="#" class="image avatar"><img src="https://www.baidu.com/img/flexible/logo/pc/result@2.png" alt="" /></a>
+				<h1><strong>I am TFriend</strong>, a super simple web<br /></h1>
+    </div>
+    
+</body>
+
+    <!-- <div class="index">首页</div> -->
 </template>
 
 <script>
