648137bce4df28ba8dbe7e8b4731d31194486721
Bodyfunk <616982274@qq.com>
2021-04-25 20:53:10 +0800

    [ADD]抽象相册组件

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-row :gutter="20" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0" class="row-con">
+          <el-col
+            :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0"
+          >
+            <el-card shadow="hover" class="card-style" :body-style="{ padding:'0px'}">
+              <el-image :src="url" :preview-src-list="srcList"> </el-image>
+              <div style="padding: 14px">
+                <span>好吃的汉堡</span>
+                <div class="bottom">
+                  <time class="time">{{ currentDate }}</time>
+                  <el-button type="text" class="button">上传</el-button>
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+</template>
+<script>
+export default {
+    data() {
+    return {
+      url:
+        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      srcList: [
+        "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
+        "https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg",
+      ],
+      currentDate: new Date(),
+    };
+  },
+}
+</script>
+<style scoped>
+.card-style {
+  margin-top: 10px;
+  height: 100%;
+}
+.row-con {
+  display: flex;
+  flex-flow: row wrap;
+}
+</style>
--- src/views/personal/Album.vue
+++ src/views/personal/Album.vue
@@ -13,43 +13,7 @@
       </div>
       <!-- 动态/相册展示 -->
       <section>
-        <el-row>
-          <el-col
-            :span="8"
-            v-for="(o, index) in 2"
-            :key="o"
-            :offset="index > 0 ? 2 : 0"
-          >
-            <el-card shadow="hover" class="card-style" :body-style="{ padding:'0px'}">
-              <el-image :src="url" :preview-src-list="srcList"> </el-image>
-              <div style="padding: 14px">
-                <span>好吃的汉堡</span>
-                <div class="bottom">
-                  <time class="time">{{ currentDate }}</time>
-                  <el-button type="text" class="button">编辑</el-button>
-                </div>
-              </div>
-            </el-card>
-          </el-col>
-          <!-- 第二行 -->
-          <el-col
-            :span="8"
-            v-for="(o, index) in 2"
-            :key="o"
-            :offset="index > 0 ? 2 : 0"
-          >
-            <el-card shadow="hover" class="card-style" :body-style="{ padding:'0px'}">
-              <el-image :src="url" :preview-src-list="srcList"> </el-image>
-              <div style="padding: 14px">
-                <span>好吃的汉堡</span>
-                <div class="bottom">
-                  <time class="time">{{ currentDate }}</time>
-                  <el-button type="text" class="button">编辑</el-button>
-                </div>
-              </div>
-            </el-card>
-          </el-col>
-        </el-row>
+        <AlbumCard />
       </section>
     </div>
   
@@ -82,18 +46,12 @@
 <script>
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar.vue';
+import AlbumCard from '@/components/AlbumCard';
 export default {
   name: "PersonalAlbum",
-  components: { Navbar, Sidebar },
+  components: { Navbar, Sidebar,AlbumCard },
   data() {
     return {
-      url:
-        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
-      srcList: [
-        "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
-        "https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg",
-      ],
-      currentDate: new Date(),
       createAlbumDialogVisible: false,
       form: {},
     };
@@ -122,5 +80,10 @@ export default {
 
 .card-style {
   margin-top: 10px;
+  height: 100%;
+}
+.row-con {
+  display: flex;
+  flex-flow: row wrap;
 }
 </style>
