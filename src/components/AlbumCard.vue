6b44a43f4573756194d25699501d4aa831500f9f
LinXiaoYi <874183200@qq.com>
2021-04-26 23:21:24 +0800

    [ADD]上传图片到相册、优化无token时跳转到登陆界面

--- src/components/AlbumCard.vue
+++ src/components/AlbumCard.vue
@@ -5,40 +5,38 @@
         <span>{{album.name}}</span>
         <div class="bottom">
             <span>{{album.introduction}}</span>
-            <el-button type="text" class="button" @click="uploadDialogVisible = true">上传</el-button>
+            <el-button type="text" class="button" @click="openUploadDialog(album.ID)">上传</el-button>
         </div>
     </div>
 </el-card>
 <el-dialog title="上传照片" v-model="uploadDialogVisible" width="60%">
     <div>只能上传 jpg/png 文件，一次性最多上传10张</div>
-    <el-upload action="https://jsonplaceholder.typicode.com/posts/" ref="upload" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-remove="handleRemove" multiple :limit="10" :file-list="fileList" :auto-upload="false">
+    <el-upload action="" ref="upload" list-type="picture-card" :on-preview="handlePictureCardPreview" :http-request="uploadRequest" multiple :limit="10" :auto-upload="false">
         <i class="el-icon-plus"></i>
     </el-upload>
-    <el-dialog v-model="dialogVisible">
-        <img width="100%" :src="dialogImageUrl" alt="" />
+    <el-dialog v-model="previewDialogVisible">
+        <img width="100%" :src="previewDialogImageUrl" alt="" />
     </el-dialog>
     <template #footer>
         <span class="dialog-footer">
-            <el-button @click="uploadDialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="uploadDialogVisible = false">确 定</el-button>
+            <el-button @click="closeUploadDialog">取 消</el-button>
+            <el-button type="primary" @click="submitUpload">开始上传</el-button>
         </span>
     </template>
 </el-dialog>
 </template>
 
 <script>
-import {
-    getCurrentInstance
-} from "vue";
+import {getCurrentInstance} from "vue";
 export default {
     components: {},
     data() {
         return {
             albums: [],
-            currentDate: new Date(),
+            currentAlbumId: '',
             uploadDialogVisible: false,
-            dialogImageUrl: "",
-            dialogVisible: false,
+            previewDialogImageUrl: "",
+            previewDialogVisible: false,
         };
     },
     beforeCreate() {
@@ -67,12 +65,61 @@ export default {
 
     },
     methods: {
-        handleRemove(file, fileList) {
-            console.log(file, fileList);
+        //打开上传弹窗
+        openUploadDialog(albumId){
+            this.currentAlbumId = albumId
+            this.uploadDialogVisible = true;
         },
+        //关闭上传弹窗
+        closeUploadDialog(){
+            //先将上传的列表清空
+            this.$refs.upload.clearFiles();
+            this.uploadDialogVisible = false;
+        },
+        //上传图片点击预览
         handlePictureCardPreview(file) {
-            this.dialogImageUrl = file.url;
-            this.dialogVisible = true;
+            this.previewDialogImageUrl = file.url;
+            this.previewDialogVisible = true;
+        },
+        //自定义上传请求
+        uploadRequest(e){
+            //自定义的上传请求，上传到ali-oss（这里是每张图片都会进到这里上传一次）
+            let file = e.file
+            //1.请求后端拿到oss验证信息
+            this.$axios.post(this.$apiConfig.getUploadKey.url, {'name':file.name})
+                .then((res)=>{
+                    if (res.status != 200 || res.data.status != 200){
+                        return
+                    }
+
+                    let oss = require('ali-oss')
+                    let client = new oss({
+                        region: res.data.region,
+                        accessKeyId: res.data.accessKeyId,
+                        accessKeySecret: res.data.accessKeySecret,
+                        bucket: res.data.bucket
+                    })
+                    let currentUrl = res.data.url
+                    client.put(currentUrl, file).then(({res,url}) => {
+                        if (res && res.status == 200) {
+                            //上传成功
+                            console.log('阿里云OSS上传文件成功回调', currentUrl);
+                            //2.上传成功给服务端回调
+                            let data = {'albumId':this.currentAlbumId,'isVideo':false,'url':currentUrl}
+                            this.$axios.post(this.$apiConfig.uploadMediaSuccess.url, data)
+                                .then((res)=>{
+                                    this.$refs.upload.handleRemove(file)
+                                })
+                        }
+                    }).catch((err) => {
+                        console.log('阿里云OSS上传文件失败回调', err);
+                    });
+                    
+                })
+        },
+        //提交上传
+        submitUpload(){
+            this.$refs.upload.submit();
         },
     },
 };
--- src/utils/http/apiConfig.js
+++ src/utils/http/apiConfig.js
@@ -1,37 +1,47 @@
 export default {
     // 账号注册
     accountRegister: {
-      method: 'POST',
-      url: 'api/v1/account/register'
+        method: 'POST',
+        url: 'api/v1/account/register'
     },
     // 账号登陆
     accountLogin: {
-      method: 'POST',
-      url: 'api/v1/account/login'
+        method: 'POST',
+        url: 'api/v1/account/login'
     },
     // 获取用户信息
     getUserInfo: {
-      method: 'POST',
-      url: 'api/v1/user/info/get'
+        method: 'POST',
+        url: 'api/v1/user/info/get'
     },
     // 更新用户信息
     updateUserInfo: {
-      method: 'POST',
-      url: 'api/v1/user/info/update'
+        method: 'POST',
+        url: 'api/v1/user/info/update'
     },
     // 获取用户头像
     getUserAvatar: {
-      method: 'POST',
-      url: 'api/v1/user/avatar/get'
+        method: 'POST',
+        url: 'api/v1/user/avatar/get'
     },
     // 获取用户所有相册
     getUserAlbums: {
-      method: 'POST',
-      url: 'api/v1/user/album/albums'
+        method: 'POST',
+        url: 'api/v1/user/album/albums'
     },
     // 获取缩略图
     getThumbnail: {
-      method: 'POST',
-      url: 'api/v1/user/album/thumbnail/get'
+        method: 'POST',
+        url: 'api/v1/user/album/thumbnail/get'
+    },
+    // 获取上传凭证
+    getUploadKey: {
+        method: 'POST',
+        url: 'api/v1/user/album/upload/key/get'
+    },
+    // 文件上传成功回调
+    uploadMediaSuccess: {
+        method: 'POST',
+        url: 'api/v1/user/album/upload/media/success'
     },
 }
